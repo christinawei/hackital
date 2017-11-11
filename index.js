2a2041cd376d53a34dfff1ab19b160e565faf960
Christina Wei <tinawei888@gmail.com>
2017-11-11 14:43:12 -0500

    funfacts

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ var app = express(); //one object for that server
 app.get('/', function(req, res){
 	res.sendfile('index.html')
 })
+app.get('/funfacts', function(req, res){
+	res.sendfile('funfacts.html')
+})
+
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
 	console.log("Server started, listening on port "+port);
