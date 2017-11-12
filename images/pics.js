098aaf6bcb3ea48a2f7943ad6f256631ef696139
Christina Wei <tinawei888@gmail.com>
2017-11-12 00:26:45 -0500

    created a js file that changes color of pics

---
+++ images/pics.js
@@ -0,0 +1,25 @@
+function hoverA(element) {
+    element.setAttribute('src', 'images/jeffrey.jpg');
+}
+function unhoverA(element) {
+    element.setAttribute('src', 'images/jeffrey2.jpg');
+}
+
+function hoverN(element) {
+    element.setAttribute('src', 'images/chris.jpg');
+}
+function unhoverN(element) {
+    element.setAttribute('src', 'images/chris2.jpg');
+}
+function hoverAl(element) {
+    element.setAttribute('src', 'images/xtina.jpg');
+}
+function unhoverAl(element) {
+    element.setAttribute('src', 'images/xtina2.jpg');
+}
+function hoverL(element) {
+    element.setAttribute('src', 'images/jess.jpg');
+}
+function unhoverL(element) {
+    element.setAttribute('src', 'images/jess2.jpg');
+}
